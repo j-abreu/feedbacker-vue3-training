202124e5470759a4a7d84eec7fb5741fcb391bfd
Jeremias Abreu <jeremias10j@gmail.com>
2022-09-22 14:12:27 -0300

    Add hooks, store and widget standby component

---
+++ widget/src/components/Icon/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
+import Chat from './Chat.vue'
+
+export default defineComponent({
+  components: {
+    Loading,
+    Copy,
+    ChevronDown,
+    Chat
+  },
+  props: {
+    name: { type: String, required: true }
+  }
+})
+</script>
---
+++ widget/src/store/index.ts
@@ -0,0 +1,56 @@
+import { reactive, readonly } from 'vue'
+
+export type StoreState = {
+  currentComponent: string
+  feedbackType: string
+  message: string
+  fingerprint: string
+  apiKey: string
+  currentPage: string
+}
+
+const initialState = {
+  currentComponent: 'SelectFeedbackType',
+  feedbackType: '',
+  message: '',
+  fingerprint: '',
+  apiKey: '',
+  currentPage: ''
+}
+
+const state = reactive<StoreState>({ ...initialState })
+
+export function setCurrentComponent(component: string): void {
+  state.currentComponent = component
+}
+
+export function setFeedbackType(feedbackType: string): void {
+  state.feedbackType = feedbackType
+}
+
+export function setMessage(message: string): void {
+  state.message = message
+}
+
+export function setCurrentPage(page: string): void {
+  state.currentPage = page
+}
+
+export function setApiKey(apiKey: string): void {
+  state.apiKey = apiKey
+}
+
+export function setFingerprint(fingerprint: string): void {
+  state.fingerprint = fingerprint
+}
+
+export function restoreState(): void {
+  setCurrentComponent(initialState.currentComponent)
+  setFeedbackType(initialState.feedbackType)
+  setMessage(initialState.message)
+  setCurrentPage(initialState.currentPage)
+  setApiKey(initialState.apiKey)
+  setFingerprint(initialState.fingerprint)
+}
+
+export default readonly(state)
